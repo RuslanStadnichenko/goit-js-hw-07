604823c2ed3a3088b32fbaa898e834c6b189eb4d
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-03-31 19:51:14 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnGeneratorColor = document.querySelector('.change-color');
+
+btnGeneratorColor.addEventListener('click', function () {
+const newColor = getRandomHexColor();
+document.body.style.backgroundColor = newColor;
+document.querySelector('.color').textContent = newColor;
+});
