ea3d027af295156bc9dfead9e7b4365adc247e78
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-04-01 16:34:06 +0300

    dz7

--- js/task-6.js
+++ js/task-6.js
@@ -4,4 +4,32 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
 
+createBtn.addEventListener('click', () => {
+  createBox();
+});
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
+
+function createBox() {
+const inputNumber = document.querySelector('#controls input[type="number"]');
+  const inputNbrValue = parseInt(inputNumber.value);
+  let initialSize = 30;
+  for (let i = 0; i < inputNbrValue; i++) { 
+    const box = document.createElement('div');
+    initialSize += 10;
+  box.style.width = `${initialSize}px`;
+  box.style.height = `${initialSize}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  
+  boxesContainer.appendChild(box);
+}
+};
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
